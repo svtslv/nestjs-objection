6b3ec21fc7d0ea45bc3ef1cd2d048b1d4f106eb8
svtslv <svtslv7@gmail.com>
2020-03-12 12:47:05 +0200

    0.3.0

--- src/objection.decorators.ts
+++ src/objection.decorators.ts
@@ -5,7 +5,7 @@ export function InjectModel(model: any, connection?: string) {
   return Inject(getObjectionModelToken(model, connection))
 }
 
-export function InjectKnex(connection?) {
+export function InjectKnex(connection?: string) {
   return Inject(getObjectionConnectionToken(connection));
 }
 
